a183127b4f7f705011c32fbd61e921a159dd905e
genatin <natin.gennadiy@mail.ru>
2022-07-27 12:44:54 +0300

    first try

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from app import app
+from generating_view import generate_view
+from flask import request
+
+post = []
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return generate_view([], '/field')
+
+@app.route('/field', methods = ['POST'])
+def field():
+    text = request.form['post']
+    post.append(text)
+    return generate_view(post, '/field', default_text='')
